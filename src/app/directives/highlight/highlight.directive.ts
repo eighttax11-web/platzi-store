6557217cab0f9409a1e56fb3e5533ae96b813f43
Cristian <eighttax11@gmail.com>
2021-03-27 10:29:04 -0600

    Mi primera directiva con parametros

--- src/app/directives/highlight/highlight.directive.ts
+++ src/app/directives/highlight/highlight.directive.ts
@@ -1,12 +1,17 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(element: ElementRef) { 
-    element.nativeElement.style.backgroundColor = 'red';
+  constructor(private element: ElementRef) { }
+
+  @Input() backgroundColor: string;
+  @Input() color: string;
+  ngOnInit(): void {
+    this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    this.element.nativeElement.style.color = this.color;
   }
 
 }
